a741b55661842d8e0c90f9a1e00f244ba9015f89
KaweVK <victorkawe07@gmail.com>
2024-06-04 15:43:07 -0300

    UPDATE 0.0.6 - #F11 Criando Migration e cadastrando um médico

--- src/main/java/med/voll/api/controler/MedicoController.java
+++ src/main/java/med/voll/api/controler/MedicoController.java
@@ -4,6 +4,7 @@ import med.voll.api.medico.DadosCadastrosMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping //post é para enviar na api
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastrosMedico dados) { //RequestBody é pra dizer que tem que o parâmetro vai puxar o corpo inteiro da requisição
         repository.save(new Medico(dados)); //método para salvar
     }
