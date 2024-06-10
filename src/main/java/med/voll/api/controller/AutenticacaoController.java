c9db311a59662298c8a498fdda58c8af85e95434
KaweVK <victorkawe07@gmail.com>
2024-06-10 15:51:12 -0300

    UPDATE 1.1.1 - #F35 Controle de Autenticação

---
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -0,0 +1,29 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.DadosAutenticacao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager; //classe própria do spring security que deve ser usada para chamar o service de autenticação por debaixo dos panos
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha()); //UsernamePasswordAuthenticationToken é um dto próprio do spring, convertendo do nosso dto para ele
+        var authentication = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,19 +2,33 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //para dizer que é uma classe de configuração e deve ser carregada
 @EnableWebSecurity //indica que vamos personalizar as configurações de segurança
 public class SecurityConfigurations { //Aqui vamos colocar as configurações do Spring Security
 
-    @Bean //expõe o retorno do método ao spring
+    @Bean //serve para exportar uma classe para o Spring, fazendo com que ele consiga carregá-la e realize a sua injeção de dependência em outras classes.
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable() //desabilita proteção contra ataques do tipo Cross-Site Request Forgery
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { //método para dizer ao Spring qual algoritmo de hash de senha utilizar
+        return new BCryptPasswordEncoder();
     }
 }
