61d318101c8ff48f5649ce1b9dddd64379e620f1
KaweVK <victorkawe07@gmail.com>
2024-06-18 15:38:44 -0300

    UPDATE 2.0.3 - #F50 Validação de consultas - Esolha de Médico Aleatório

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -3,7 +3,27 @@ package med.voll.api.domain.medico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> { //JpaRepository define a entidade a usar e o tipo da chave primária
     Page<Medico> findAllByAtivoTrue(Pageable paginacao); //o nome escrito corretamente facilita para que a consulta seja criada automaticamente
+
+    @Query("""
+            select m from Medico m
+            where
+            m.ativo = 1
+            and
+            m.especialidade = :especialidade
+            and
+            m.id not in(
+                select c.medico.id from Consulta c
+                where
+                c.data = :data
+            )
+            order by rand()
+            limit 1
+            """)//query é usado para implementar consulta no banco de dados
+    Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
 }
